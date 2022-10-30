a7e190d4b2718104a46f5090f2a14a0a1315d189
Cas De Smet <cas.desmet@student.hogent.be>
2022-10-30 14:07:22 +0100

    Initial Commit

---
+++ QuizApp/QuizAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuizAppApp.swift
+//  QuizApp
+//
+//  Created by Cas De Smet on 30/10/2022.
+//
+
+import SwiftUI
+
+@main
+struct QuizAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
